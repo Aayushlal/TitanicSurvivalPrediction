6ecdf3e55fbcfebb61e16edc813ab6f0c92c58a3
Aayushlal <98844237+Aayushlal@users.noreply.github.com>
2024-03-04 00:09:37 +0530

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-df = pd.read_csv('tested.csv')
+df = pd.read_csv('Titanic-Dataset.csv')
 
 df.head()
 
